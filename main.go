d6064b24d585e5c7408e0bc9e4200ea58b7e6045
fufel <mikhailovskiid@gmail.com>
2024-03-06 13:35:45 +0300

    hotfix v0.1.2

--- main.go
+++ main.go
@@ -6,19 +6,11 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 	n := ""
-=======
-	n := 0
->>>>>>> master
-	fmt.Print("Введите целое число: ")
+	fmt.Print("Введите данные: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
-<<<<<<< HEAD
-	fmt.Printf("Вы ввели число: %v\n", n)
-=======
-	fmt.Printf("Вы ввели число: %d\n", n)
->>>>>>> master
+	fmt.Printf("Вы ввели следующие данные: %v\n", n)
 }
